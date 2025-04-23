2d617ca5db6143eed1676d3c3a289e19b21035e8
IMOWWW <imowww@yandex.ru>
2025-04-23 15:29:35 +0500

    fix

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
       {
         name: "comiccon_frontend",         // Название вашего приложения
         script: "node_modules/next/dist/bin/next", // путь к бинарнику next
-        args: "start -p 3012",        // запуск команды `next start` на порту 3002
+        args: "start -p 3000",        // запуск команды `next start` на порту 3002
         instances: "max",             // можно указать число экземпляров или "max" для кластерного режима
         exec_mode: "cluster",         // режим запуска (cluster или fork)
         env: {
