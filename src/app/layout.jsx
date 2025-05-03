7009bb7b1c2f966b6b7331f2892eb8be620af95a
uzhojiakbar <uzhojiakbar3@gmail.com>
2025-05-03 05:29:12 +0500

    coming sone ready

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -62,6 +62,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+
         <ClientProviders>{children}</ClientProviders>
       </body>
     </html>
